da68f58a86b8d9b92e56e24e01757839692003ae
Pollus Brodeur <pollusb@gmail.com>
2025-02-25 22:14:58 -0500

    working clone.ps1

--- repo/clone.ps1
+++ repo/clone.ps1
@@ -1,2 +1,12 @@
-# will clone the repo, but it should only fetch latest
-git clone https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit.git "$PSScriptRoot\frk"
+# clone the repo, but keep only relevent stuff
+
+Remove-Item $PSScriptRoot\frk.old -Recurse -Force -ErrorAction SilentlyContinue
+Rename-Item $PSScriptRoot\frk frk.old             -ErrorAction SilentlyContinue
+git clone https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit.git $PSScriptRoot\frk
+Get-ChildItem $PSScriptRoot\frk -Exclude sp_Blitz*.sql -Recurse | Remove-Item -Recurse -Confirm:$false
+
+# get version
+
+$M = Get-Content $PSScriptRoot\frk\sp_Blitz.sql | Select-String "SELECT @Version = '([\d\.]+)', @VersionDate = '(\d+)'"
+$content = 'Version {0} Date {1}' -f $M.Matches.Groups[1].Value, [datetime]::ParseExact($M.Matches.Groups[2].Value, 'yyyyMMdd', $null).ToString('yyyy-MM-dd')
+$content | Set-Content -Path $PSScriptRoot\frk\version.txt
