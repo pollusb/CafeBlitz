5166bc3f3adbcebb37c1eefcb125641d0720b0d8
Pollus Brodeur <pollus.brodeur@lautorite.qc.ca>
2025-03-25 14:23:35 -0400

    sp_Blitz.temp upgrade to 8.23

--- repo/clone.ps1
+++ repo/clone.ps1
@@ -1,14 +1,14 @@
-# setp 1 - after a new First Responder Kit revision is made availabe
+# after a new First Responder Kit revision is made availabe
 # clone repo in frk, but keep only relevent stuff
-# keep the old version in frk.old to be able to compare
+# keep the old version in frk.old to compare
 
 Remove-Item $PSScriptRoot\frk.old -Recurse -Force -ErrorAction SilentlyContinue
 Rename-Item $PSScriptRoot\frk frk.old             -ErrorAction SilentlyContinue
 git clone https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit.git $PSScriptRoot\frk
-Get-ChildItem $PSScriptRoot\frk -Exclude sp_Blitz*.sql -Recurse | Remove-Item -Recurse -Confirm:$false
+Get-ChildItem $PSScriptRoot\frk -Exclude sp_Blitz*.sql -Include .git -Recurse | Remove-Item -Recurse -Confirm:$false
+Get-ChildItem .git -Recurse -Attributes H | Remove-Item -Force -Confirm:$false
 
 # get version
-
 $M = Get-Content $PSScriptRoot\frk\sp_Blitz.sql | Select-String "SELECT @Version = '([\d\.]+)', @VersionDate = '(\d+)'"
 $content = 'Version {0} Date {1}' -f $M.Matches.Groups[1].Value, [datetime]::ParseExact($M.Matches.Groups[2].Value, 'yyyyMMdd', $null).ToString('yyyy-MM-dd')
 $content | Set-Content -Path $PSScriptRoot\frk\version.txt
--- repo/integrate.ps1
+++ repo/integrate.ps1
@@ -1,5 +1,5 @@
 # this code will integrate FRK code into the project
-# TODO: should force a new branch ?
+# TODO: should force a new branch or check that this is not main?
 
 # pseudocode
 # foreach script file from our integration
@@ -8,17 +8,22 @@
 
 # sp_blitz
 $path = "$PSScriptRoot\frk\sp_blitz.sql"
-$dest = Resolve-Path "$PSScriptRoot\..\public\tsql\sp_Blitz.temp.sql"
+$destTemp = (Get-ChildItem "$PSScriptRoot\..\sp_Blitz.temp.sql" -Recurse).FullName
 if (Test-Path $path) {
     $code = Get-Content -Path $path -Raw
     $pattern = "(?s)IF OBJECT_ID.*ALTER PROCEDURE \[dbo\]\.\[sp_Blitz\]"
     if ($code -match $pattern) {
         $code = $code -replace $pattern, "-- CafeBlitz integration`r`n`r`nCREATE PROCEDURE [dbo].[#sp_Blitz]"
-        $code | Set-Content -Path $dest
+        Copy-Item $destTemp "$destTemp.bak" -ErrorAction SilentlyContinue
+        $code | Set-Content -Path $destTemp
+
+        # sp_blitz.param
+        $destParam = (Get-ChildItem "$PSScriptRoot\..\sp_Blitz.param.sql" -Recurse).FullName
+        Copy-Item $destParam "$destParam.bak" -ErrorAction SilentlyContinue
     }
     else {
-        Write-Warning "sp_blitz source code was not modified.`ncode --diff '$path' '$dest'"
+        Write-Warning "sp_blitz source code was not modified because pattern was not found.`ncode --diff '$path' '$destTemp'"
     }
-
-    $dest
+    $destTemp
 }
+else { throw "$path not found. Was there a problem with the clone or the file was deprecated?" }
