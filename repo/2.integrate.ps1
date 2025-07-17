f4f86da88d9e2080c9b73ad8267c95baa281672c
Pollus Brodeur <pollusb@gmail.com>
2025-07-17 15:36:46 -0400

    2-integration is now working

--- repo/2.integrate.ps1
+++ repo/2.integrate.ps1
@@ -13,6 +13,7 @@ $dest = Resolve-Path "$PSScriptRoot\..\CafeBlitz\tsql" -RelativeBasePath $PSScri
 $frk = Resolve-Path "$PSScriptRoot\frk" -RelativeBasePath $PSScriptRoot -Relative
 
 #region sp_Blitz
+
 $name = 'sp_Blitz'
 $path = "$frk\$name.sql"
 $temp = "$dest\$name.temp.sql"
@@ -24,26 +25,30 @@ if (Test-Path $path) {
     $with = "CREATE PROCEDURE [dbo].[#$name]"
     if ($code -match $pattern) {
         $code -replace $pattern, $with | Out-File -FilePath $temp -Encoding utf8
-        $status = 'Success'
     }
     else {
-        $status = 'Fail'
+        Write-Warning 'oups 1'
     }
-    $pattern = '(?s)ALTER PROCEDURE \w+\.\w+\s+(.*)WITH RECOMPILE\s+AS'
+
+    $pattern = "(?s)ALTER PROCEDURE \[dbo\]\.\[$name\]\s+(.*)WITH RECOMPILE\s+AS"
     $code = Get-Content -Path $path -Raw
     if ($code -match $pattern) {
         $Matches[1] -split "`r`n" -replace '^\s+' | Out-File -FilePath $param -Encoding utf8
     }
+    else {
+        Write-Warning 'oups 1'
+    }
     [PSCustomObject]@{
         Name      = $name
-        Status    = $status
         PathTemp  = $temp
         PathParam = $param
     }
 }
+
 #endregion
 
 #region sp_BlitzCache
+
 $name = 'sp_BlitzCache'
 $path = "$frk\$name.sql"
 $temp = "$dest\$name.temp.sql"
@@ -54,26 +59,29 @@ if (Test-Path $path) {
     $with = "PROCEDURE [dbo].[#$name]"
     if ($code -match $pattern) {
         $code -replace $pattern, $with | Out-File -FilePath $temp -Encoding utf8
-        $status = 'Success'
     }
     else {
-        $status = 'Fail'
+        Write-Warning 'oups 2'
     }
     $pattern = '(?s)ALTER PROCEDURE \w+\.\w+\s+(.*)WITH RECOMPILE\s+AS'
     $code = Get-Content -Path $path -Raw
     if ($code -match $pattern) {
         $Matches[1] -split "`r`n" -replace '^\s+' | Out-File -FilePath $param -Encoding utf8
     }
+    else {
+        Write-Warning 'oups 2'
+    }
     [PSCustomObject]@{
         Name      = $name
-        Status    = $status
         PathTemp  = $temp
         PathParam = $param
     }
 }
+
 #endregion
 
 #region sp_BlitzIndex
+
 $name = 'sp_BlitzIndex'
 $path = "$frk\$name.sql"
 $temp = "$dest\$name.temp.sql"
@@ -84,26 +92,26 @@ if (Test-Path $path) {
     $with = "PROCEDURE [dbo].[#$name]"
     if ($code -match $pattern) {
         $code = $code -replace $pattern, $with
-        $code = $code -replace 'OBJECT_ID(''dbo.sp_BlitzIndex'')','OBJECT_ID(''tempdb.dbo.#sp_BlitzIndex'')'
+        $code = $code -replace 'OBJECT_ID(''dbo.sp_BlitzIndex'')', 'OBJECT_ID(''tempdb.dbo.#sp_BlitzIndex'')'
         $code | Out-File -FilePath $temp -Encoding utf8
-
-        $status = 'Success'
     }
     else {
-        $status = 'Fail'
+        Write-Warning 'oups 3'
     }
 
-    $pattern = '(?s)ALTER PROCEDURE \w+\.\w+\s+(.*)WITH RECOMPILE\s+AS'
+    $pattern = "(?s)ALTER PROCEDURE \[*dbo\]*\.\[*$name\]\s+(.*)WITH RECOMPILE\s+AS"
     $code = Get-Content -Path $path -Raw
     if ($code -match $pattern) {
         $Matches[1] -split "`r`n" -replace '^\s+' | Out-File -FilePath $param -Encoding utf8
     }
+    else {
+        Write-Warning 'oups 3'
+    }
     [PSCustomObject]@{
         Name      = $name
-        Status    = $status
         PathTemp  = $temp
         PathParam = $param
-
     }
 }
+
 #endregion
